c5f710b1237b257d3bfe529e47ef137a1f494dda
BrunoW <06071726069@senacrs.edu.br>
2025-11-12 09:45:39 -0300

    yeh

---
+++ projetos/obj test/obj.js
@@ -0,0 +1,87 @@
+// Lista de Exercícios – Objetos em JavaScript
+// 1. Criando um objeto simples
+// Crie um objeto chamado pessoa com as propriedades nome, idade e cidade.
+// Depois, exiba todas as informações no console.
+
+const pessoa = {
+    nome: `Bruno`,
+    idade: 16,
+    cidade: `Santa Cruz do Sul`
+}
+
+console.log(pessoa.nome, pessoa.idade, pessoa.cidade)
+
+// 2. Atualizando informações
+// Usando o objeto pessoa do exercício anterior:
+// Altere a cidade para “Santa Cruz do Sul RS”.
+// Adicione uma nova propriedade chamada profissao.
+// Mostre o resultado final no console.
+
+pessoa.cidade = `Santa Cruz do Sul RS`
+pessoa.profissao = `Sim`
+console.log(pessoa.nome, pessoa.idade, pessoa.cidade, pessoa.profissao)
+
+// 3. Objeto com função
+// Crie um objeto carro com as propriedades marca, modelo, ano e uma função ligar() que mostre no console:
+// “O carro [marca] [modelo] foi ligado!”
+// Execute o método.
+
+const carro = {
+  marca: 'Renault',
+  modelo: 'KWID',
+  ano: 2026,
+  ligar() {
+    console.log(`O carro ${this.marca} ${this.modelo} foi ligado!`);
+  }
+};
+
+carro.ligar(); 
+
+// 4. Usando if dentro de um método
+// Crie um objeto usuario com as propriedades nome e idade.
+// Adicione um método chamado verificarIdade() que:
+// Mostre “Maior de idade” se a idade for 18 ou mais.
+// Mostre “Menor de idade” caso contrário.
+
+
+
+// 5. Objeto com array
+// Crie um objeto aluno com:
+// nome,
+// notas (array com 3 números),
+// e um método media() que calcule e retorne a média.
+// Mostre a média no console.
+
+
+// 6. Laço com array de objetos
+// Crie um array chamado produtos com 3 objetos, cada um contendo nome e preco.
+// Use um for para mostrar o nome e o preço de cada produto.
+
+
+// 7. Condicional em objetos
+// Com base no array produtos do exercício anterior, mostre apenas os produtos com preço maior que 50.
+
+
+// 8. Função que recebe um objeto
+// Crie uma função chamada mostrarFilme(filme) que recebe um objeto com titulo e ano e mostre:
+// “O filme [titulo] foi lançado em [ano].”
+// Chame a função passando um objeto criado por você.
+
+
+// 9. Objeto dentro de objeto
+// Crie um objeto empresa com:
+// nome,
+// endereco (outro objeto com rua, numero, cidade).
+// Mostre no console o nome da empresa e o nome da rua.
+
+
+// 10. Controle de estoque
+// Crie um objeto produto com as propriedades:
+// nome,
+// preco,
+// estoque,
+// e um método chamado vender() que:
+// Diminua o estoque em 1 cada vez que for chamado.
+// Mostre no console:
+// “Venda realizada! Estoque atual: [estoque].”
+// Teste o método várias vezes e veja o estoque diminuindo.
